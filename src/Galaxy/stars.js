28dda9d06edc9e6cf1b7fd16ae5783bff0abec15
cnywt <923759010@qq.com>
2018-04-08 11:40:36 +0800

    Update stars.js

--- src/Galaxy/stars.js
+++ src/Galaxy/stars.js
@@ -1,1 +1,14 @@
-
+class PointStars {
+        constructor(){
+        }
+        initPoints(){
+          let pa=[];
+          for(let i=0;i<starNums;i++){
+             let x=(Math.random()-0.5)*showArea;
+             let y=(Math.random()-0.5)*showArea;
+             let z=(Math.random()-0.5)*showArea;
+             pa.push(x,y,x);
+          }
+          
+        }
+} 
